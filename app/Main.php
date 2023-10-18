bce5f4f33c4044a18f0dd656804dff46426251d3
Paul Hendrick <paul.g.hendrick@gmail.com>
2023-10-17 22:13:38 -0400

    🎨 cleanup day 1

--- app/Main.php
+++ app/Main.php
@@ -6,10 +6,23 @@ class Main
 {
     public static function first($in)
     {
+        $c = self::process($in);
+        sort($c);
+        return array_slice($c, -1)[0];
+    }
+
+    public static function second($in)
+    {
+        $c = self::process($in);
+        sort($c);
+        return array_sum(array_slice($c, -3));
+    }
+
+    private static function process($in)
+    {
         $a = explode("\n\n", $in);
         $b = array_map(fn ($i) => explode("\n", $i), $a);
         $c = array_map(fn ($i) => array_sum($i), $b);
-        sort($c);
-        return array_reverse($c)[0];
+        return $c;
     }
 }

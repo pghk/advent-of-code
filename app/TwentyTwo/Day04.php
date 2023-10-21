8ab1ab18c0b715e28e89e6e3e56d5ba0d79fa475
Paul Hendrick <paul.g.hendrick@gmail.com>
2023-10-21 18:13:26 -0400

    ♻️ Cleanup Day 4

--- app/TwentyTwo/Day04.php
+++ app/TwentyTwo/Day04.php
@@ -4,23 +4,39 @@ namespace App\TwentyTwo;
 
 class Day04
 {
-    public static function partOne($input)
-    {
-        $pairs = array_map(function ($i) {
-            return explode(',', $i);
-        }, $input);
 
+    public static function conditionOne($a, $b): bool
+    {
+        return ($a[0] <= $b[0] && $a[1] >= $b[1]) ||
+            ($a[0] >= $b[0] && $a[1] <= $b[1]);
+    }
+
+    public static function conditionTwo($a, $b): bool
+    {
+        return ($a[0] >= $b[0] && $a[0] <= $b[1]) ||
+            ($a[1] >= $b[0] && $a[1] <= $b[1]) ||
+            ($b[0] >= $a[0] && $b[0] <= $a[1]) ||
+            ($b[1] >= $a[0] && $b[1] <= $a[1]);
+    }
+
+    public static function partOne($input): int
+    {
+        return self::run($input, self::conditionOne(...));
+    }
+
+    public static function partTwo($input): int
+    {
+        return self::run($input, self::conditionTwo(...));
+    }
+
+    private static function run($input, $check): int
+    {
+        $pairs = self::parseInput($input);
         $count = 0;
 
-        foreach ($pairs as $i) {
-            [$a, $b] = array_map(function ($i) {
-                $i = explode('-', $i);
-                return $i;
-            }, $i);
-            if (
-                ($a[0] <= $b[0] && $a[1] >= $b[1]) ||
-                ($a[0] >= $b[0] && $a[1] <= $b[1])
-            ) {
+        foreach ($pairs as $p) {
+            [$a, $b] = self::splitPair($p);
+            if ($check($a, $b)) {
                 $count++;
             }
         }
@@ -28,29 +44,25 @@ class Day04
         return $count;
     }
 
-    public static function partTwo($input)
+    /**
+     * @param $input
+     * @return false[]|string[][]
+     */
+    private static function parseInput($input): array
     {
-        $pairs = array_map(function ($i) {
+        return array_map(function ($i) {
             return explode(',', $i);
         }, $input);
+    }
 
-        $count = 0;
-
-        foreach ($pairs as $i) {
-            [$a, $b] = array_map(function ($i) {
-                $i = explode('-', $i);
-                return $i;
-            }, $i);
-            if (
-                ($a[0] >= $b[0] && $a[0] <= $b[1]) ||
-                ($a[1] >= $b[0] && $a[1] <= $b[1]) ||
-                ($b[0] >= $a[0] && $b[0] <= $a[1]) ||
-                ($b[1] >= $a[0] && $b[1] <= $a[1])
-            ) {
-                $count++;
-            }
-        }
-
-        return $count;
+    /**
+     * @param array|false $i
+     * @return array
+     */
+    public static function splitPair(array|false $i): array
+    {
+        return array_map(function ($i) {
+            return explode('-', $i);
+        }, $i);
     }
 }

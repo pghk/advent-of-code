5a51057b74273894db8970d5ea8e7f85a1a5a3e9
Paul Hendrick <paul@hendrick.dev>
2023-12-02 14:25:15 -0500

    ✅ 2023 Day 2, part 1

---
+++ app/TwentyThree/Day02.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\TwentyThree;
+
+class Day02
+{
+    private $games;
+
+    public function __construct(public array $input)
+    {
+       $this->games = $this->parse($input);
+    }
+
+    public function partOne()
+    {
+        $limits = [
+            "red" => 12,
+            "green" => 13,
+            "blue" => 14,
+        ];
+
+        $possibleGames = [];
+
+        foreach ($this->games as $g) {
+            $possible = true;
+            $i = 0;
+            while ($possible && $i < count($g->sets)) {
+                $set = $g->sets[$i];
+                foreach ($limits as $color => $max) {
+                    if ($set->$color > $max) {
+                        $possible = false;
+                        break;
+                    }
+                }
+                $i++;
+            }
+            if ($possible) {
+                $possibleGames[] = $g->id;
+            }
+        }
+
+        return array_sum($possibleGames);
+    }
+
+    // public function partTwo()
+    // {
+    //
+    // }
+
+    private function parse(array $defs)
+    {
+        return array_map(fn ($d) => new Game($d), $defs);
+    }
+}
+
+class Game {
+
+    public int $id;
+    public array $sets;
+
+    public function __construct($def)
+    {
+        [$name, $data] = explode(': ', $def);
+        $this->id = intval(str_replace('Game ', '', $name));
+        $this->sets = array_map(fn ($d) => new Set($d), explode('; ', $data));
+    }
+}
+
+class Set {
+    public int $red = 0;
+    public int $green = 0;
+    public int $blue = 0;
+
+    public function __construct(string $def)
+    {
+        foreach (explode(', ', $def) as $cube) {
+            [$size, $color] = explode(' ', $cube);
+            $this->$color = $size;
+        }
+    }
+}
---
+++ tests/TwentyThree/Day02.test.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\TwentyThree\Day02;
+
+const EXAMPLE_02 = <<<txt
+    Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+    Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+    Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+    Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+    txt;
+
+test('Part One: Example', function () {
+    $input = explode("\n", EXAMPLE_02);
+    $sys = new Day02($input);
+    expect($sys->partOne())->toBe(8);
+});
+
+ test('Part One: Puzzle', function () {
+     $input = file(at('TwentyThree/02.txt'), FILE_IGNORE_NEW_LINES);
+     $sys = new Day02($input);
+     expect($sys->partOne())->toBe(2377);
+ });
+
+// test('Part Two: Example', function () {
+//     $input = explode("\n", EXAMPLE_02);
+//     $sys = new Day02($input);
+//     expect($sys->partTwo())->toBe(0);
+// });
+//
+// test('Part Two: Puzzle', function () {
+//     $input = file(at('TwentyThree/02.txt'), FILE_IGNORE_NEW_LINES);
+//     $sys = new Day02($input);
+//     expect($sys->partTwo())->toBe(0);
+// });

88e4c2e4154b9dec1baf17e1ac0656cce8c8e3f4
Paul Hendrick <paul@hendrick.dev>
2023-12-02 22:20:41 -0500

    ✅ 2022: Day 13, part 1

---
+++ app/TwentyTwo/Day13.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\TwentyTwo;
+
+use phpDocumentor\Reflection\Type;
+
+class Day13
+{
+    private array $pairs;
+    public function __construct(public array $input)
+    {
+       $this->pairs = array_map(
+           fn ($i) => array_map(
+               fn ($j) => json_decode($j),
+               explode("\n", $i)
+           ),
+           $input
+       );
+    }
+
+    public function partOne()
+    {
+        $correctlyOrdered = [];
+        foreach($this->pairs as $i => $pair) {
+            [$a, $b] = $pair;
+            [$c, $d] = $pair;
+            if($this->compareLists($a, $b) == -1) {
+                print("correct: " . json_encode($c) . " vs " . json_encode($d) . "\n");
+                $correctlyOrdered[] = $i + 1;
+            } else {
+                print("incorrect: " . json_encode($c) . " vs " . json_encode($d) . "\n");
+            }
+        }
+//        return join(', ', $correctlyOrdered);
+        return array_sum($correctlyOrdered);
+    }
+
+    // public function partTwo()
+    // {
+    //
+    // }
+
+
+    private function compareLists(&$one, &$two)
+    {
+        print("comparing lists: " . json_encode($one) . " & " . json_encode($two) . "\n");
+        if (count($two) && !count($one)) {
+            return -1;
+        }
+        if (count($one) && !count($two)) {
+            return 1;
+        }
+        if (!count($one) && !count($two)) {
+            return 0;
+        }
+
+        $a = array_shift($one);
+        $b = array_shift($two);
+
+        $delta = null;
+
+        if (gettype($a) == 'array' && gettype($b) == 'array') {
+            print("lists: " . json_encode([$a, $b]) . "\n");
+            $delta = $this->compareLists($a, $b);
+        }
+        if ($delta === 0) {
+            return $this->compareLists($one, $two);
+        }
+        if ((gettype($a) == 'integer' && gettype($b) == 'integer') || (gettype($a) == 'integer' && $b === null) || (gettype($a) == 'integer' && $b === null)) {
+            print("ints: " . json_encode([$a, $b]) . "\n");
+            $delta = $this->compareIntegers($a, $b);
+        }
+        if ($delta === 0) {
+            return $this->compareLists($one, $two);
+        }
+        if ($delta === null) {
+            [$a, $b] = $this->normalize($a, $b);
+            return $this->compareLists($a, $b);
+        }
+
+        print("delta {$delta}: " . json_encode([$a, $b]) . "\n");
+
+
+        return $delta;
+    }
+
+    private function compareIntegers($a, $b): int
+    {
+        print("comparing ints: " . json_encode($a) . " & " .json_encode($b) . "\n");
+       if ($a == $b) {
+           return 0;
+       }
+       return ($a < $b) ? -1 : 1;
+    }
+
+    private function normalize($a, $b): array
+    {
+        print("normalizing " . json_encode($a) . " & " . json_encode($b) . "\n");
+        if (gettype($a) == gettype($b)) {
+            return [$a, $b];
+        }
+        return match (gettype($a)) {
+            'integer' => [[$a], $b],
+            'array' => [$a, [$b]],
+        };
+    }
+}
---
+++ tests/TwentyTwo/Day13.test.php
@@ -0,0 +1,53 @@
+<?php
+
+use App\TwentyTwo\Day13;
+
+const EXAMPLE_13 = <<<txt
+    [1,1,3,1,1]
+    [1,1,5,1,1]
+
+    [[1],[2,3,4]]
+    [[1],4]
+
+    [9]
+    [[8,7,6]]
+
+    [[4,4],4,4]
+    [[4,4],4,4,4]
+
+    [7,7,7,7]
+    [7,7,7]
+
+    []
+    [3]
+
+    [[[]]]
+    [[]]
+
+    [1,[2,[3,[4,[5,6,7]]]],8,9]
+    [1,[2,[3,[4,[5,6,0]]]],8,9]
+    txt;
+
+test('Part One: Example', function () {
+    $input = explode("\n\n", EXAMPLE_13);
+    $sys = new Day13($input);
+    expect($sys->partOne())->toBe(13);
+});
+
+ test('Part One: Puzzle', function () {
+     $input = explode("\n\n", file_get_contents(at('TwentyTwo/13.txt')));
+     $sys = new Day13($input);
+     expect($sys->partOne())->toBe(6568);
+ });
+
+// test('Part Two: Example', function () {
+//     $input = explode("\n", EXAMPLE_13);
+//     $sys = new Day13($input);
+//     expect($sys->partTwo())->toBe(0);
+// });
+//
+// test('Part Two: Puzzle', function () {
+//     $input = file(at('TwentyTwo/13.txt'), FILE_IGNORE_NEW_LINES);
+//     $sys = new Day13($input);
+//     expect($sys->partTwo())->toBe(0);
+// });

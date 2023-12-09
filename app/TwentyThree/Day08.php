027659e0b8d9b06ae80e1fd592d2f425fe47953b
Paul Hendrick <paul@hendrick.dev>
2023-12-09 14:35:32 -0500

    ✨ Day 08, part 2

--- app/TwentyThree/Day08.php
+++ app/TwentyThree/Day08.php
@@ -19,7 +19,7 @@ class Day08
         $this->instructions = str_split($input[0]);
         $ids = [];
         $dirs = [];
-        foreach(array_slice($this->input, 2) as $line) {
+        foreach (array_slice($this->input, 2) as $line) {
             [$id, $dir] = explode(' = ', $line);
             $ids[] = $id;
             $dirs[] = array_combine(
@@ -30,7 +30,7 @@ class Day08
         $this->map = array_combine($ids, $dirs);
     }
 
-    public function partOne()
+    public function partOne(): int
     {
         $steps = 0;
         $pos = 'AAA';
@@ -43,8 +43,45 @@ class Day08
         return $steps;
     }
 
-    // public function partTwo()
-    // {
-    //
-    // }
+    public function partTwo(): int
+    {
+        $nodes = array_keys($this->map);
+        $pos_ids = array_filter($nodes, fn($k) => str_ends_with($k, 'A'));
+
+        $positions = array_values($pos_ids);
+        $steps = array_fill(0, count($positions), 0);
+
+        foreach ($positions as $k => $v) {
+            $pos = $v;
+            $step = 0;
+            while (!str_ends_with($pos, 'Z')) {
+                foreach ($this->instructions as $i) {
+                    $pos = $this->map[$pos][$i];
+                    $step++;
+                }
+                $steps[$k] = $step;
+            }
+        }
+
+        $lcm = 1;
+        foreach ($steps as $s) {
+            $lcm = $this->lcm($lcm, $s);
+        }
+        return $lcm;
+    }
+
+    private function gcd($a, $b): int
+    {
+        $m = $a % $b;
+        if ($m == 0) {
+            return $b;
+        }
+        return $this->gcd($a, $m);
+    }
+
+    private function lcm($a, $b): int
+    {
+        return $a * $b / $this->gcd($a, $b);
+    }
+
 }
--- tests/TwentyThree/Day08.test.php
+++ tests/TwentyThree/Day08.test.php
@@ -22,6 +22,19 @@ const EXAMPLE_08b = <<<txt
     ZZZ = (ZZZ, ZZZ)
     txt;
 
+const EXAMPLE_08c = <<<txt
+    LR
+
+    11A = (11B, XXX)
+    11B = (XXX, 11Z)
+    11Z = (11B, XXX)
+    22A = (22B, XXX)
+    22B = (22C, 22C)
+    22C = (22Z, 22Z)
+    22Z = (22B, 22B)
+    XXX = (XXX, XXX)
+    txt;
+
 test('Part One: Example A', function () {
     $input = explode("\n", EXAMPLE_08a);
     $sys = new Day08($input);
@@ -34,20 +47,20 @@ test('Part One: Example B', function () {
     expect($sys->partOne())->toBe(6);
 });
 
- test('Part One: Puzzle', function () {
-     $input = file(at('TwentyThree/08.txt'), FILE_IGNORE_NEW_LINES);
-     $sys = new Day08($input);
-     expect($sys->partOne())->toBe(12599);
- });
+test('Part One: Puzzle', function () {
+    $input = file(at('TwentyThree/08.txt'), FILE_IGNORE_NEW_LINES);
+    $sys = new Day08($input);
+    expect($sys->partOne())->toBe(12599);
+});
 
-// test('Part Two: Example', function () {
-//     $input = explode("\n", EXAMPLE_08);
-//     $sys = new Day08($input);
-//     expect($sys->partTwo())->toBe(0);
-// });
-//
-// test('Part Two: Puzzle', function () {
-//     $input = file(at('TwentyThree/08.txt'), FILE_IGNORE_NEW_LINES);
-//     $sys = new Day08($input);
-//     expect($sys->partTwo())->toBe(0);
-// });
+test('Part Two: Example', function () {
+    $input = explode("\n", EXAMPLE_08c);
+    $sys = new Day08($input);
+    expect($sys->partTwo())->toBe(6);
+});
+
+test('Part Two: Puzzle', function () {
+    $input = file(at('TwentyThree/08.txt'), FILE_IGNORE_NEW_LINES);
+    $sys = new Day08($input);
+    expect($sys->partTwo())->toBe(8245452805243);
+});
